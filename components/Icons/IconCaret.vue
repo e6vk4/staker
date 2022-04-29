055aa0edafddada50bf47c0b7e286e88a47fc30a
Ebuka Ezeh <ebuka@ezeh.me>
2022-04-29 08:09:24 +0100

    add: Caret icon component

---
+++ components/Icons/IconCaret.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="flex-none inline w-auto ml-3 text-gray-500 transition-transform duration-300 transform h2 hover:text-primary title"
+    data-v-ba8e3b54=""
+    style="height: 1rem"
+  >
+    <path
+      d="M12 6L8.35355 9.64645C8.15829 9.84171 7.84171 9.84171 7.64645 9.64645L4 6"
+      stroke="#6F7590"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
